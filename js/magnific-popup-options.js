124add1c67244a66cb148965355af032fbcc321c
Haniya konain <114371721+haniyakonain@users.noreply.github.com>
2024-09-07 00:27:49 +0530

    Add files via upload

---
+++ js/magnific-popup-options.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+	var magnifPopup = function() {
+		$('.image-popup').magnificPopup({
+			type: 'image',
+			removalDelay: 300,
+			mainClass: 'mfp-with-zoom',
+			product:{
+				enabled:true
+			},
+			zoom: {
+				enabled: true,
+
+				duration: 300, 
+				easing: 'ease-in-out', 
+
+			
+				opener: function(openerElement) {
+			
+				return openerElement.is('img') ? openerElement : openerElement.find('img');
+				}
+			}
+		});
+	};
+
+	var magnifVideo = function() {
+		$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+	};
+
+	
+
+
+	// Call the functions 
+	magnifPopup();
+	magnifVideo();
+
+});
